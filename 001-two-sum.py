37e6a8de2a98b6385f0b56d7359d7483c487f16f
AdrianHsu <adrianhsu1995@gmail.com>
2018-02-19 13:00:35 +0800

    :tada: first commit

---
+++ 001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+  def twoSum(self, nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+
+s = Solution()
+print(s.twoSum([3, 2, 4], 6))
+print(s.twoSum([2, 7, 11, 15], 9))
